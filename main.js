c0e2106d286234cdb11b0604cf671c6d98ad6583
Lwam Tekie <ltekie87@yahoo.com>
2020-03-31 01:05:47 -0500

    wip

---
+++ main.js
@@ -0,0 +1,29 @@
+
+// variable and data types 
+
+
+// var firstName = 'John';
+// console.log(firstName);
+
+// var lastName = 'Smith';
+// var age = 28;
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job);
+
+// job = 'Teacher';
+// console.log(job);
+
+// var e3years = 3;
+
+
+//*************************************
+// variable mutations and type coercion 
+
+
+var firstName = 'John';
+var age = 28;
+console.log(firstName + '' + age);
